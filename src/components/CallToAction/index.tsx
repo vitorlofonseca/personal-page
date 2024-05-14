b925a4dffdbe4fb02957efd3e74d12d7c6bda251
Vitor Fonseca <vitorlofonseca@hotmail.com>
2024-05-14 20:47:19 +0100

    Call to action redirecting to link

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -3,11 +3,19 @@ import Button from "@mui/material/Button";
 import EastIcon from "@mui/icons-material/East";
 
 export const CallToAction = ({ size = "large" }) => {
+  function openCourse() {
+    window.open(
+      "https://www.udemy.com/course/mastering-web-performance-optimization/?couponCode=FONSECAS-GIFT",
+      "_blank"
+    );
+  }
+
   return (
     <Button
       variant="contained"
       className={`${styles.callToAction} ${styles["callToAction--" + size]}`}
       endIcon={<EastIcon />}
+      onClick={openCourse}
     >
       Check course!
     </Button>
